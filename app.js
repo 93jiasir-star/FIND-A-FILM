2aebbea9589f7ff378e10a4009299a47b103bc41
佳Sir <93jiasir@gmail.com>
2026-08-03 11:42:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const platforms = [
   { group: '云上集', note: '网盘资源', icon: '盘', name: '盘搜', meta: '纯净的夸克网盘资源搜索引擎', url: q => `https://pansou.de/search?q=${q}&platform=quark` },
   { group: '云上集', note: '网盘资源', icon: '追', name: '追剧网', meta: '国内外热门影视网盘资源搜索', url: q => `https://www.zhuiju.us/s/${q}.html` },
   { group: '云上集', note: '网盘资源', icon: 'K', name: 'KKSO', meta: '夸克百度网盘搜索', url: q => `https://kkso.net/s/${q}.html` },
-  { group: '云上集', note: '网盘资源', icon: '资', name: '资源库', meta: '打开后自行输入访问密码', url: () => 'http://xccji.top/app/index.html?id=200317xlb' }
+  { group: '云上集', note: '网盘资源', icon: '资', name: '资源库', meta: '7 7 8 8', url: () => 'http://xccji.top/app/index.html?id=200317xlb' }
 ];
 
 const form = document.querySelector('#searchForm');
