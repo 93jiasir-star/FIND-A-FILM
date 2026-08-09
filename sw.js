7dca6a9f4e438b650aeae37a79622aefab11c7ac
佳Sir <93jiasir@gmail.com>
2026-08-09 20:54:35 +0800

    Publish final Find a Film icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'find-a-film-v52';
-const ASSETS = ['./', './index.html', './style.css?v=25', './app.js?v=42', './manifest.webmanifest', './icon-1024.png','./xuan-paper.png'];
+const CACHE = 'find-a-film-v53';
+const ASSETS = ['./', './index.html', './style.css?v=25', './app.js?v=42', './manifest.webmanifest', './icon-1024.png?v=2','./xuan-paper.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => { if (event.request.method === 'GET' && new URL(event.request.url).origin === location.origin) event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))); });
