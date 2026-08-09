d78e0343bc3e2374ae02df0a1ea435aaff3d0796
佳Sir <93jiasir@gmail.com>
2026-08-09 10:58:19 +0800

    刷新拾影页脚缓存

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'find-a-film-v39';
-const ASSETS = ['./', './index.html', './style.css?v=15', './app.js?v=33', './manifest.webmanifest', './icon-1024.png','./xuan-paper.png'];
+const CACHE = 'find-a-film-v40';
+const ASSETS = ['./', './index.html', './style.css?v=16', './app.js?v=34', './manifest.webmanifest', './icon-1024.png','./xuan-paper.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => { if (event.request.method === 'GET' && new URL(event.request.url).origin === location.origin) event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))); });
